fb67b61cad629817fd480f1ec789ea785fe89dd6
Darma Yasa <darmayasa@192.168.204.104>
2022-08-05 15:22:25 +0800

    implement animes domain

--- src/Domains/animes/entities/Anime.ts
+++ src/Domains/animes/entities/Anime.ts
@@ -1,1 +1,29 @@
-export default class Anime {}
+interface iAnime {
+  id: string;
+  coverImage: {
+    color: string;
+    size: {
+      extraLarge: string;
+      large: string;
+      medium: string;
+    };
+  };
+  title: object;
+}
+
+export default class Anime implements iAnime {
+  public id: string;
+
+  public coverImage: {
+    color: string;
+    size: { extraLarge: string; large: string; medium: string };
+  };
+
+  public title: object;
+
+  constructor(payload: iAnime) {
+    this.id = payload.id;
+    this.coverImage = payload.coverImage;
+    this.title = payload.title;
+  }
+}
--- src/Domains/animes/entities/AnimeDetail.ts
+++ src/Domains/animes/entities/AnimeDetail.ts
@@ -1,1 +1,30 @@
-export default class AnimeDetail {}
+interface iAnimeDetail {
+  id: string;
+  bannerImage: string;
+  title: object;
+  description: string;
+  episodes: string;
+  genres: Array<string>;
+}
+export default class AnimeDetail implements iAnimeDetail {
+  public id: string;
+
+  public bannerImage: string;
+
+  public title: object;
+
+  public description: string;
+
+  public episodes: string;
+
+  public genres: string[];
+
+  constructor(payload: iAnimeDetail) {
+    this.id = payload.id;
+    this.bannerImage = payload.bannerImage;
+    this.title = payload.title;
+    this.description = payload.description;
+    this.episodes = payload.episodes;
+    this.genres = payload.genres;
+  }
+}
