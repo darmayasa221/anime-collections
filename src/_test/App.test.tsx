5ac5ca60cba858df87a0c672ec0e0dbfc32a2ae7
Darma Yasa <darmayasa@192.168.204.104>
2022-08-05 13:44:06 +0800

    ci

--- src/_test/App.test.tsx
+++ src/_test/App.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
+import '@testing-library/jest-dom';
 
 describe('test', () => {
   it('should render hello world', async () => {
