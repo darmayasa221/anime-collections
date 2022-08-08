354610289660852cb5a21180b1feccad9bf878de
Darma Yasa <darmayasa@192.168.204.104>
2022-08-09 02:55:38 +0800

    implemenet repository service apollo

--- src/Infrastructures/repository/apollo/AnimeRepositoryApollo.ts
+++ src/Infrastructures/repository/apollo/AnimeRepositoryApollo.ts
@@ -1,3 +1,80 @@
 import AnimeRepository from '../../../Domains/animes/AnimeRepository';
+import { apolloClient, gql } from '../../api/apollo/ApolloClient';
 
-export default class AnimeRepositoryApollo extends AnimeRepository {}
+export default class AnimeRepositoryApollo extends AnimeRepository {
+  private client: typeof apolloClient;
+
+  private gqlQuery: typeof gql;
+
+  constructor(client: typeof apolloClient, gqlQuery: typeof gql) {
+    super();
+    this.client = client;
+    this.gqlQuery = gqlQuery;
+  }
+
+  async getAnimes(): Promise<any> {
+    const { data } = await this.client.query({
+      query: this.gqlQuery(`
+       {
+        Page(perPage: 10) {
+           media {
+            id
+            title {
+              romaji
+              english
+              native
+              userPreferred
+            }
+            coverImage {
+              color
+              extraLarge
+              large
+              medium
+              color
+            }
+          }
+        }
+      }
+      `),
+    });
+
+    return data.Page.media;
+  }
+
+  async getAnimeById(id: number): Promise<any> {
+    if (typeof id !== 'number') {
+      throw new Error('id not found');
+    }
+    const { data, error } = await this.client.query({
+      query: this.gqlQuery(`
+      query anime($id:Int){
+        Media(id: $id, type: ANIME) {
+          id
+          coverImage {
+            extraLarge
+            large
+            medium
+            color
+          }
+          title {
+            romaji
+            english
+            native
+            userPreferred
+          }
+          bannerImage
+          description
+          episodes
+          genres
+          averageScore
+        }
+      }
+      `),
+      variables: { id },
+    });
+    if (error) {
+      throw new Error(error.message);
+    }
+    return data.Media;
+  }
+}
